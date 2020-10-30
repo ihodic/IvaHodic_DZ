6742be8a433016535e1942c92d07d9a98b8465f8
ihodic <72932363+ihodic@users.noreply.github.com>
2020-10-30 18:53:25 +0100

    Add files via upload

---
+++ DZ1/ClassLibrary1/Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+	public class Episode
+	{
+			public int viewers;
+			public double totalScore;
+			public double maxScore;
+			double max = 0;
+
+			public Episode(int aViewers, double aTotalScore, double aMaxScore)
+			{
+				viewers = aViewers;
+				totalScore = aTotalScore;
+				maxScore = aMaxScore;
+			}
+
+			public Episode()
+			{
+			}
+
+
+			public void AddView(double aScore)
+			{
+				double score = aScore;
+				totalScore += score;
+				if (score > max)
+				{
+					max = score;
+				}
+			}
+			public double GetMaxScore()
+			{
+				return max;
+			}
+
+			public double GetAverageScore()
+			{
+				return totalScore / viewers;
+			}
+
+
+			public double GetViewerCount()
+			{
+				return 10;
+			}
+		}
+}
